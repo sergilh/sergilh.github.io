c4cb700be90ba48e15316c54566f9c86434162e3
Sergi López <serkio.lh@gmail.com>
2026-01-21 19:56:11 +0100

    Create redteam.js

---
+++ js/redteam.js
@@ -0,0 +1,48 @@
+const red = [ 
+  {
+    title: "Google Cybersecurity Labs",
+    description: "Laboratorios y actividades de redacción profesional documentadas relacionadas con la ciberseguridad.",
+    tags: ["Alerts", "Reports", "Linux","SQL"],
+    github: "https://github.com/sergilh/sergilh.github.io/tree/ab25f545e0e36ea8f387de03f0c45dc07b33e939/labs/Google%20Cybersecurity", 
+  },
+  {
+    title: "🔐 Challenge Final – File Upload & Node.js RCE",
+    description: "Objetivo: Comprometer la máquina objetivo obteniendo una shell y localizar la flag final",
+    tags: ["Node.js", "RCE", "FileUpload"],
+    github: "https://github.com/sergilh/sergilh.github.io/blob/2129b1f79ba2a8a53d4d13e1ee446f68413d7ea7/labs/redteam/1-%20FileUpload%20Node.js%20-%20RCE.txt",
+  },
+  {
+    title: "Pickle Rick - CI PrivEsc",
+    description: "Objetivo: Ayudar a Rick obteniendo los tres ingredientes ocultos en el sistema.",
+    tags: ["Enum", "Command Injection", "PrivEsc"],
+    github: "https://github.com/sergilh/sergilh.github.io/blob/2129b1f79ba2a8a53d4d13e1ee446f68413d7ea7/labs/redteam/2-%20Pickle%20Rick%20-%20CI%20PrivEsc.txt", 
+  },
+  {
+    title: "Rabbit Store – JWT Abuse, SSTI & Privilege Escalation",
+    description: "Objetivo: Comprometer la máquina y obtener acceso root.",
+    tags: ["JWT", "SSTI", "PrivEsc"],
+    github: "https://github.com/sergilh/sergilh.github.io/blob/2129b1f79ba2a8a53d4d13e1ee446f68413d7ea7/labs/redteam/3-%20Rabbit%20Store%20-%20JWT%20PrivEsc", 
+  }
+];
+
+const container = document.getElementById("red-container");
+
+red.forEach(r => {
+  const card = document.createElement("article");
+  card.className = "red-card";
+
+  card.innerHTML = `
+    <h3>${r.title}</h3>
+    <p>${r.description}</p>
+
+    <div class="tags">
+      ${r.tags.map(tag => `<span>${tag}</span>`).join("")}
+    </div>
+
+    <div class="links">
+      ${r.github ? `<a href="${r.github}" target="_blank">GitHub</a>` : ""}
+    </div>
+  `;
+
+  container.appendChild(card);
+});
