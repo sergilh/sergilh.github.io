016150cd5f63769d3642b8a012b82f24c9e4a8d0
Sergi López <serkio.lh@gmail.com>
2026-01-21 18:22:13 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -10,7 +10,19 @@ const projects = [
     description: "Administración de usuarios, grupos y configuraciones del sistema en entornos Windows y Linux",
     tags: ["Linux", "SysAdmin", "Users"],
     github: "https://github.com/sergilh/sergilh.github.io/tree/f2a7f31a34a4c3f566cda9bad6f33038412bd3fb/projects/2-Configuraci%C3%B3n-SO-UOC",
-  }
+  },
+  {
+    title: "Proyecto académico - Servicios, Procesos, Conexión a Red y Optimización del Sistema Operativo",
+    description: "Análisis, gestión y optimización de los procesos y servicios del sistema operativo y configuración correcta de la conectividad de red entre máquinas virtuales",
+    tags: ["Windows", "Ubuntu", "Linux","VirtualBox"],
+    github: "https://github.com/sergilh/sergilh.github.io/tree/46f24ce45486b1fb6f85d2a7c4eee3996ff23748/projects/3-Optimizaci%C3%B3n-SO-UOC", 
+  },
+  {
+    title: "Proyecto académico - Gestión de Archivos y Almacenamiento",
+    description: "Comprensión del funcionamiento de los sistemas de archivos, las particiones, y los métodos de gestión de almacenamiento en diferentes sistemas operativos",
+    tags: ["Windows", "Ubuntu", "Linux","VirtualBox"],
+    github: "https://github.com/sergilh/sergilh.github.io/tree/46f24ce45486b1fb6f85d2a7c4eee3996ff23748/projects/4-Gesti%C3%B3n-Archivos-Almacen.-UOC", 
+  },
 ];
 
 const container = document.getElementById("projects-container");
